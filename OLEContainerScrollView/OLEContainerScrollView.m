062fe32b3dbcd6ad3ed31417b0e68223b48091d0
Nick Quaranto <nick@quaran.to>
2014-10-17 13:30:02 -0400

    Support UIWebView as a subview as well

--- OLEContainerScrollView/OLEContainerScrollView.m
+++ OLEContainerScrollView/OLEContainerScrollView.m
@@ -60,6 +60,10 @@ static void *KVOContext = &KVOContext;
         UIScrollView *scrollView = (UIScrollView *)subview;
         scrollView.scrollEnabled = NO;
         [scrollView addObserver:self forKeyPath:NSStringFromSelector(@selector(contentSize)) options:NSKeyValueObservingOptionOld context:KVOContext];
+    } else if ([subview isKindOfClass:[UIWebView class]]) {
+        UIScrollView *scrollView = [(UIWebView *)subview scrollView];
+        scrollView.scrollEnabled = NO;
+        [scrollView addObserver:self forKeyPath:NSStringFromSelector(@selector(contentSize)) options:NSKeyValueObservingOptionOld context:KVOContext];
     } else {
         [subview addObserver:self forKeyPath:NSStringFromSelector(@selector(frame)) options:NSKeyValueObservingOptionOld context:KVOContext];
         [subview addObserver:self forKeyPath:NSStringFromSelector(@selector(bounds)) options:NSKeyValueObservingOptionOld context:KVOContext];
@@ -74,6 +78,8 @@ static void *KVOContext = &KVOContext;
     
     if ([subview isKindOfClass:[UIScrollView class]]) {
         [subview removeObserver:self forKeyPath:NSStringFromSelector(@selector(contentSize)) context:KVOContext];
+    } else if ([subview isKindOfClass:[UIWebView class]]) {
+        [[(UIWebView *)subview scrollView] removeObserver:self forKeyPath:NSStringFromSelector(@selector(contentSize)) context:KVOContext];
     } else {
         [subview removeObserver:self forKeyPath:NSStringFromSelector(@selector(frame)) context:KVOContext];
         [subview removeObserver:self forKeyPath:NSStringFromSelector(@selector(bounds)) context:KVOContext];
@@ -164,6 +170,42 @@ static void *KVOContext = &KVOContext;
             scrollView.frame = frame;
             scrollView.contentOffset = contentOffset;
 
+            yOffsetOfCurrentSubview += scrollView.contentSize.height;
+        } else if ([subview isKindOfClass:[UIWebView class]]) {
+            UIWebView *webView = (UIWebView *)subview;
+            UIScrollView *scrollView = webView.scrollView;
+            CGRect frame = webView.frame;
+            CGPoint contentOffset = scrollView.contentOffset;
+            
+            // Translate the logical offset into the sub-scrollview's real content offset and frame size.
+            // Methodology:
+            
+            // (1) As long as the sub-scrollview has not yet reached the top of the screen, set its scroll position
+            // to 0.0 and position it just like a normal view. Its content scrolls naturally as the container
+            // scroll view scrolls.
+            if (self.contentOffset.y < yOffsetOfCurrentSubview) {
+                contentOffset.y = 0.0;
+                frame.origin.y = yOffsetOfCurrentSubview;
+            }
+            // (2) If the user has scrolled far enough down so that the sub-scrollview reaches the top of the
+            // screen, position its frame at 0.0 and start adjusting the sub-scrollview's content offset to
+            // scroll its content.
+            else {
+                contentOffset.y = self.contentOffset.y - yOffsetOfCurrentSubview;
+                frame.origin.y = self.contentOffset.y;
+            }
+            
+            // (3) The sub-scrollview's frame should never extend beyond the bottom of the screen, even if its
+            // content height is potentially much greater. When the user has scrolled so far that the remaining
+            // content height is smaller than the height of the screen, adjust the frame height accordingly.
+            CGFloat remainingBoundsHeight = fmax(CGRectGetMaxY(self.bounds) - CGRectGetMinY(frame), 0.0);
+            CGFloat remainingContentHeight = fmax(scrollView.contentSize.height - contentOffset.y, 0.0);
+            frame.size.height = fmin(remainingBoundsHeight, remainingContentHeight);
+            frame.size.width = self.contentView.bounds.size.width;
+            
+            webView.frame = frame;
+            scrollView.contentOffset = contentOffset;
+            
             yOffsetOfCurrentSubview += scrollView.contentSize.height;
         }
         else {
